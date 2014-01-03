21f4213b8936ed7cfe1f31602c999fdc787f1cd2
Cristina <cristina@conekta.mx>
2014-01-03 03:58:27 -0600

    mes

--- app/models/spree/payment_decorator.rb
+++ app/models/spree/payment_decorator.rb
@@ -1,8 +1,8 @@
 module Spree
   Payment.class_eval do
-    state_machine :initial => 'checkout' do
-      after_transition :to => 'completed', :do => :create_subscriptions!
-    end
+    #state_machine :initial => 'checkout' do
+    #  after_transition :to => 'completed', :do => :create_subscriptions!
+    #end
 
     def create_subscriptions!
       self.order.create_subscriptions
